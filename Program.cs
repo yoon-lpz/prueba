1a728fe5710e9dc6d49af646fd37a805ccf7495b
Yoon Lopez <yoon.lopez.7e9@itb.cat>
2025-10-23 12:10:20 +0200

    prueba

--- Program.cs
+++ Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace IsNaturalNumber
+{
+    public class IsNaturalNumber
+    {
+        public static void Main()
+        {
+            const string MsgInput = "Enter a value to check if it's a natural number:";
+            const string MsgOk = " is a natural number.";
+            const string MsgKo = " is NOT a natural number.";
+
+            int value;
+            bool isNatural;
+
+            Console.WriteLine(MsgInput);
+            value = int.Parse(Console.ReadLine());
+
+            if (value >= 0)
+            {
+                isNatural = true;
+            }
+            else
+            {
+                isNatural = false;
+            }
+
+            if (isNatural)
+            {
+                Console.WriteLine(value + MsgOk);
+            }
+            else
+            {
+                Console.Write(value + MsgKo);
+            }
+        }
+    }
+}
